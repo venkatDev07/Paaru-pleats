c5ccbf59b5e7edbf11eceed7f700892a23c0fbb5
paru <kagitha239@gmail.com>
2026-08-13 15:32:49 +0530

    admin login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -9,9 +9,9 @@ export const login = asyncHandler(async (req, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: isProduction,
+    secure: isProduction, // true when NODE_ENV=production
     sameSite: isProduction ? "none" : "lax",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
   res.status(200).json(new ApiResponse(200, { admin }, "Login successful"));

26ea0d5d4ff17ffa4ed0a76acca401cc5039aa68
paru <kagitha239@gmail.com>
2026-08-13 14:59:10 +0530

    Restore Swagger JSDoc comments on auth and work routes

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -18,14 +18,11 @@ const adminSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-// Hash password before saving, only if it changed
-adminSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+adminSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
-// Instance method to compare login password with hashed one
 adminSchema.methods.comparePassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };

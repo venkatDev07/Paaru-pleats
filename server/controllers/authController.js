5f069d705eef515d8d99143d4c38d0d8ef844d90
paru <kagitha239@gmail.com>
2026-08-25 09:38:24 +0530

    versel.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,12 +7,12 @@ const isProduction = process.env.NODE_ENV === "production";
 export const login = asyncHandler(async (req, res) => {
   const { token, admin } = await authService.loginAdmin(req.body);
 
-  // res.cookie("token", token, {
-  //   httpOnly: true,
-  //   secure: true, // true when NODE_ENV=production
-  //   sameSite: "none",
-  //   maxAge: 7 * 24 * 60 * 60 * 1000,
-  // });
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true, // true when NODE_ENV=production
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
 
   res.status(200).json(new ApiResponse(200, { admin }, "Login successful"));
 });
